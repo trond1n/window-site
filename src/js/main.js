2c3cdf7be39c2ca889cb52e6460b2db5f4ae3d63
Yuri Trondin <lopata@2ch.hk>
2020-10-08 22:12:51 +0500

    сделал формы

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,18 @@
-import './slider';
-import modals from './modules/modals';
-import tabs from './modules/tabs';
+import "./slider";
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
+import forms from "./modules/forms";
 
-window.addEventListener('DOMContentLoaded', () => {
-    modals();
-    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
-    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
-
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  modals();
+  tabs(".glazing_slider ", ".glazing_block", ".glazing_content", "active");
+  tabs(
+    ".decoration_slider",
+    ".no_click",
+    ".decoration_content > div > div",
+    "after_click"
+  );
+  forms();
 });
 // console.log(3);
