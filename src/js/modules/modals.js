3498278ddc8af86807e8d233a2e7553ce24d1707
Yuri Trondin <lopata@2ch.hk>
2020-10-09 18:43:04 +0500

    поправил баги, добавил анимацию

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -8,7 +8,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector);
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
-    const windows = document.querySelectorAll("[data-modal]");
+    const windows = document.querySelectorAll("[data-modal]"),
+      scroll = calcScroll();
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
@@ -20,6 +21,8 @@ const modals = () => {
         });
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`;
+
         // document.body.classList.add('modal-open');
       });
     });
@@ -30,6 +33,8 @@ const modals = () => {
       });
       modal.style.display = "none";
       document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`;
+
       // document.body.classList.remove('modal-open');
     });
 
@@ -40,6 +45,8 @@ const modals = () => {
         });
         modal.style.display = "none";
         document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`;
+
         // document.body.classList.remove('modal-open');
       }
     });
@@ -51,6 +58,19 @@ const modals = () => {
       document.body.style.overflow = "hidden";
     }, time);
   }
+
+  function calcScroll() {
+    let div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
+
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
